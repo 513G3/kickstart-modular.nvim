ac95331ed1b8243b5c0c2c4f159f0db6ae8ddb34
CJ <cj@513g3.mooo.com>
2024-01-07 07:31:30 -0700

    Remove spurious newline

--- lua/lsp-setup.lua
+++ lua/lsp-setup.lua
@@ -91,7 +91,6 @@ capabilities = require('cmp_nvim_lsp').default_capabilities(capabilities)
 
 -- Ensure the servers above are installed
 local mason_lspconfig = require 'mason-lspconfig'
-
 mason_lspconfig.setup {
   ensure_installed = vim.tbl_keys(servers),
 }
