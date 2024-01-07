11ec347fd254b982fd0ad34d98a9ec1e67d0fafb
CJ <cj@513g3.mooo.com>
2024-01-07 07:15:48 -0700

    Add neo-tree

---
+++ lua/custom/plugins/neo-tree.lua
@@ -0,0 +1,31 @@
+return {
+  "nvim-neo-tree/neo-tree.nvim",
+  version = "*",
+  dependencies = {
+    "nvim-lua/plenary.nvim",
+    "nvim-tree/nvim-web-devicons", -- not strictly required, but recommended
+    "MunifTanjim/nui.nvim",
+  },
+  config = function()
+    vim.cmd([[ let g:neo_tree_remove_legacy_commands = 1 ]])
+    vim.keymap.set('n', '<leader>tt', ':Neotree reveal toggle<cr>', { desc = 'toggle Neotree' })
+
+    -- Because we have explicitly defined config(), we cannot
+    -- use 'opts' at the outer layer since the toplevel's init.lua
+    -- will not call setup() for us.  Since we want some options
+    -- to be passed to setup() and override their default values,
+    -- we need to call setup() ourselves and write out the options
+    -- that we want to override:
+    require('neo-tree').setup({
+      filesystem = {
+        filtered_items = {
+          -- visible = true, -- Show hidden files by default
+        },
+        follow_current_file = {
+          enabled = true, -- Refocus the tree as nvim bounces around files
+        },
+        use_libuv_file_watcher = true, -- Auto-update the tree as files are added and removed
+      },
+    })
+  end,
+}
