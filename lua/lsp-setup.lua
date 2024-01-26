e2f399c9765804b184f0a2bc729c12b172d90eff
513G3 <29024145+513G3@users.noreply.github.com>
2024-01-26 09:26:32 -0700

    Light up the buf formatter

--- lua/lsp-setup.lua
+++ lua/lsp-setup.lua
@@ -77,10 +77,11 @@ require('mason-tool-installer').setup {
 require("conform").setup({
   formatters_by_ft = {
     --lua = { "stylua" },
-    -- Conform will run multiple formatters sequentially
+    proto = { "buf" },
     python = { "isort" },
     -- Use a sub-list to run only the first available formatter
     --javascript = { { "prettierd", "prettier" } },
+    -- Conform will run multiple formatters sequentially
     sh = { "beautysh", "shellcheck" },
   },
   format_on_save = {
