9c644ee1170e2cfac2ce9bf98c54203b3f4d6ad3
513G3 <29024145+513G3@users.noreply.github.com>
2024-02-02 19:54:38 -0700

    Disable some cpplint complaints

--- lua/custom/plugins/none-ls.lua
+++ lua/custom/plugins/none-ls.lua
@@ -1,12 +1,12 @@
 return {
-  'nvimtools/none-ls.nvim',
+  "nvimtools/none-ls.nvim",
   dependencies = {
-    'nvim-lua/plenary.nvim',
+    "nvim-lua/plenary.nvim",
   },
   config = function()
-    local null_ls = require 'null-ls'
+    local null_ls = require("null-ls")
 
-    null_ls.setup {
+    null_ls.setup({
       sources = {
         -- Formatters
         null_ls.builtins.formatting.beautysh,
@@ -16,13 +16,13 @@ return {
         -- Linters
         null_ls.builtins.diagnostics.buf,
         null_ls.builtins.diagnostics.checkmake,
-        null_ls.builtins.diagnostics.cpplint,
+        null_ls.builtins.diagnostics.cpplint.with({ args = { "--filter=-legal/copyright,-whitespace/braces", "$FILENAME" } }),
         null_ls.builtins.diagnostics.jsonlint,
         null_ls.builtins.diagnostics.protolint,
         -- null_ls.builtins.diagnostics.shellcheck (already used by bash_language_server)
       },
-    }
+    })
 
-    vim.keymap.set('n', '<leader>f', vim.lsp.buf.format, { desc = 'Format' })
+    vim.keymap.set("n", "<leader>f", vim.lsp.buf.format, { desc = "Format" })
   end,
 }
