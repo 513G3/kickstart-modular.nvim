64889ab01569b52c62c97b601fefb55e93c575bd
513G3 <29024145+513G3@users.noreply.github.com>
2024-02-27 16:41:38 -0700

    Stop using deprecated cpplint builtin in none-ls

--- lua/custom/plugins/none-ls.lua
+++ lua/custom/plugins/none-ls.lua
@@ -1,30 +1,34 @@
 return {
-  "nvimtools/none-ls.nvim",
+  'nvimtools/none-ls.nvim',
   dependencies = {
-    "nvim-lua/plenary.nvim",
+    'nvimtools/none-ls-extras.nvim',
+    'nvim-lua/plenary.nvim',
   },
   config = function()
-    local null_ls = require("null-ls")
+    local null_ls = require('null-ls')
 
-    null_ls.setup({
+    null_ls.setup {
       sources = {
-        -- Formatters
+        -- Built-in formatters
         null_ls.builtins.formatting.buf,
         null_ls.builtins.formatting.isort,
         null_ls.builtins.formatting.shfmt,
         null_ls.builtins.formatting.stylua,
-        -- Linters
+
+        -- Built-in linters
         null_ls.builtins.diagnostics.buf,
         null_ls.builtins.diagnostics.checkmake,
-        null_ls.builtins.diagnostics.cpplint.with({
-          args = { "--filter=-legal/copyright,-whitespace/braces,-whitespace/newline", "$FILENAME" },
-        }),
         null_ls.builtins.diagnostics.jsonlint,
         null_ls.builtins.diagnostics.protolint,
         -- null_ls.builtins.diagnostics.shellcheck (already used by bash_language_server)
-      },
-    })
 
-    vim.keymap.set("n", "<leader>f", vim.lsp.buf.format, { desc = "Format" })
+        -- Extra linters
+        require('none-ls.diagnostics.cpplint').with({
+          args = { '--filter=-legal/copyright,-whitespace/braces,-whitespace/newline', '$FILENAME' },
+        }),
+      },
+    }
+
+    vim.keymap.set('n', '<leader>f', vim.lsp.buf.format, { desc = 'Format' })
   end,
 }
