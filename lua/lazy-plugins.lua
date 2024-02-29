9577e7e60654b890fe9fa6d06b3794149196734f
513G3 <29024145+513G3@users.noreply.github.com>
2024-02-28 19:55:32 -0700

    Remove original inclusion of lualine

--- lua/lazy-plugins.lua
+++ lua/lazy-plugins.lua
@@ -59,20 +59,6 @@ require('lazy').setup({
   },
 
   {
-    -- Set lualine as statusline
-    'nvim-lualine/lualine.nvim',
-    -- See `:help lualine.txt`
-    opts = {
-      options = {
-        icons_enabled = false,
-        theme = 'onedark',
-        component_separators = '|',
-        section_separators = '',
-      },
-    },
-  },
-
-  {
     -- "gc" to comment visual regions/lines
     'numToStr/Comment.nvim',
     opts = {},
