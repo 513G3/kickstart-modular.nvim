10cf34f37ad7f0e7bd98f0422e0b0a375246ac1a
513G3 <29024145+513G3@users.noreply.github.com>
2024-02-28 19:58:44 -0700

    More tweaks to get closer to matching upstream/master

--- lua/lazy-plugins.lua
+++ lua/lazy-plugins.lua
@@ -91,6 +91,10 @@ require('lazy').setup({
 
   require 'kickstart/plugins/which-key',
 
+  -- require 'kickstart/plugins/telescope',
+
+  -- require 'kickstart/plugins/lspconfig',
+
   -- require 'kickstart/plugins/conform',
 
   require 'kickstart/plugins/cmp',
@@ -102,10 +106,20 @@ require('lazy').setup({
   require 'kickstart/plugins/treesitter',
 
   require 'kickstart/plugins/indent-blankline',
+  --
+  -- The following two comments only work if you have downloaded the kickstart repo, not just copy pasted the
+  -- init.lua. If you want these files, they are in the repository, so you can just download them and
+  -- put them in the right spots if you want.
 
-  -- require 'kickstart/plugins/debug',
+  -- NOTE: Next step on your Neovim journey: Add/Configure additional plugins for kickstart
+  --
+  --  Here are some example plugins that I've included in the kickstart repository.
+  --  Uncomment any of the lines below to enable them (you will need to restart nvim).
+  --
+  -- require 'kickstart.plugins.debug',
+  -- require 'kickstart.plugins.indent_line',
 
--- NOTE: The import below can automatically add your own plugins, configuration, etc from `lua/custom/plugins/*.lua`
+  -- NOTE: The import below can automatically add your own plugins, configuration, etc from `lua/custom/plugins/*.lua`
   --    This is the easiest way to modularize your config.
   --
   --  Uncomment the following line and add your plugins to `lua/custom/plugins/*.lua` to get going.
