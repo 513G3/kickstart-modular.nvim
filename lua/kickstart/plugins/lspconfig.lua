acbee57922bf4685fe8bedb964b0b3e88ed9b2e0
513G3 <29024145+513G3@users.noreply.github.com>
2025-03-08 18:45:36 -0700

    Update LSP stuff

--- lua/kickstart/plugins/lspconfig.lua
+++ lua/kickstart/plugins/lspconfig.lua
@@ -138,11 +138,12 @@ return {
       --  - settings (table): Override the default settings passed when initializing the server.
       --        For example, to see the options for `lua_ls`, you could go to: https://luals.github.io/wiki/settings/
       local servers = {
-        bufls = { filetypes = { 'proto' } },
+        buf = { filetypes = { 'proto' } },
         bashls = { filetypes = { 'sh' } },
         clangd = { filetypes = { 'c', 'cpp' } },
         jsonls = { filetypes = { 'json' } },
-        ruff_lsp = { filetypes = { 'python' } },
+        ruff = { filetypes = { 'python' } },
+        taplo = { filetypes = { 'toml' } },
         pyright = {
           filetypes = { 'python' },
           settings = {
@@ -201,7 +202,6 @@ return {
         'checkmake',
         'clang-format',
         'cpplint',
-        'isort',
         'jsonlint',
         'protolint',
         'shellcheck',
