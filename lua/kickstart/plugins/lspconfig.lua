7d14c924b721aba0787ace8e49cc85150560078b
513G3 <29024145+513G3@users.noreply.github.com>
2025-05-19 20:25:16 -0600

    Add border for hover

--- lua/kickstart/plugins/lspconfig.lua
+++ lua/kickstart/plugins/lspconfig.lua
@@ -95,7 +95,11 @@ return {
 
           -- Opens a popup that displays documentation about the word under your cursor
           --  See `:help K` for why this keymap
-          map('K', vim.lsp.buf.hover, 'Hover Documentation')
+          map(
+            'K',
+            function() vim.lsp.buf.hover { border = 'single', max_height = 25, max_width = 120 } end,
+            'Hover Documentation'
+          )
 
           -- WARN: This is not Goto Definition, this is Goto Declaration.
           --  For example, in C this would take you to the header
