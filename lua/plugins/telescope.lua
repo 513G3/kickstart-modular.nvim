8ed9da3b35f5c7911a9794e270f92ef72fbf18f7
513G3 <29024145+513G3@users.noreply.github.com>
2026-05-18 21:11:40 -0600

    Remove old comments

--- lua/plugins/telescope.lua
+++ lua/plugins/telescope.lua
@@ -1,109 +1,39 @@
--- NOTE: Plugins can specify dependencies.
---
--- The dependencies are proper plugin specifications as well - anything
--- you do for a plugin at the top level, you can do for a dependency.
---
--- Use the `dependencies` key to specify the dependencies of a particular plugin
-
 return {
-  { -- Fuzzy Finder (files, lsp, etc)
+  {
     'nvim-telescope/telescope.nvim',
     event = 'VeryLazy',
     dependencies = {
       'nvim-lua/plenary.nvim',
-      { -- If encountering errors, see telescope-fzf-native README for install instructions
+      {
         'nvim-telescope/telescope-fzf-native.nvim',
-
-        -- `build` is used to run some command when the plugin is installed/updated.
-        -- This is only run then, not every time Neovim starts up.
         build = 'make',
-
-        -- `cond` is a condition used to determine whether this plugin should be
-        -- installed and loaded.
         cond = function()
           return vim.fn.executable 'make' == 1
         end,
       },
       { 'nvim-telescope/telescope-ui-select.nvim' },
-
-      -- Useful for getting pretty icons, but requires special font.
-      --  If you already have a Nerd Font, or terminal set up with fallback fonts
-      --  you can enable this
       { 'nvim-tree/nvim-web-devicons' },
     },
     config = function()
-      -- Telescope is a fuzzy finder that comes with a lot of different things that
-      -- it can fuzzy find! It's more than just a "file finder", it can search
-      -- many different aspects of Neovim, your workspace, LSP, and more!
-      --
-      -- The easiest way to use telescope, is to start by doing something like:
-      --  :Telescope help_tags
-      --
-      -- After running this command, a window will open up and you're able to
-      -- type in the prompt window. You'll see a list of help_tags options and
-      -- a corresponding preview of the help.
-      --
-      -- Two important keymaps to use while in telescope are:
-      --  - Insert mode: <c-/>
-      --  - Normal mode: ?
-      --
-      -- This opens a window that shows you all of the keymaps for the current
-      -- telescope picker. This is really useful to discover what Telescope can
-      -- do as well as how to actually do it!
-
-      -- [[ Configure Telescope ]]
-      -- See `:help telescope` and `:help telescope.setup()`
       require('telescope').setup {
-        -- You can put your default mappings / updates / etc. in here
-        --  All the info you're looking for is in `:help telescope.setup()`
-        --
-        -- defaults = {
-        --   mappings = {
-        --     i = { ['<c-enter>'] = 'to_fuzzy_refine' },
-        --   },
-        -- },
 
         pickers = {
           find_files = {
             find_command = {
-              'rg',                -- Use ripgrep (make sure it is installed!)
+              'rg',
               '--files-without-match',
-              '\\.(pyc|o)$',       -- Ignore intermediates
-              -- '-g',
-              -- '!venv',             -- Ignore venv glob
-              -- '-g',
-              -- '!nodejs',           -- Ignore nodejs glob
-              -- '-g',
-              -- '!node_modules',     -- Ignore node_modules glob
-              -- '-g',
-              -- '!gtest',            -- Ignore gtest glob
-              -- '--no-ignore-vcs',   -- Do not ignore stuff that VCS ignores
+              '\\.(pyc|o)$',
             },
-            follow = true,         -- Follow symlinks; not sure why "--follow"
-                                   -- does not work above
+            follow = true,
           },
           grep_string = {
             additional_args = {
-              -- '--iglob',
-              -- '!compile_commands', -- Ignore compile_commands glob
-              -- '--iglob',
-              -- '!venv',             -- Ignore venv glob
-              -- '--iglob',
-              -- '!gtest',            -- Ignore gtest glob
-              -- '--no-ignore-vcs',   -- Do not ignore stuff that VCS ignores
-              '--follow',          -- Follow symlinks
+              '--follow',
             },
           },
           live_grep = {
             additional_args = {
-              -- '--iglob',
-              -- '!compile_commands', -- Ignore compile_commands glob
-              -- '--iglob',
-              -- '!venv',             -- Ignore venv glob
-              -- '--iglob',
-              -- '!gtest',            -- Ignore gtest glob
-              -- '--no-ignore-vcs',   -- Do not ignore stuff that VCS ignores
-              '--follow',          -- Follow symlinks
+              '--follow',
             },
           },
         },
@@ -114,12 +44,8 @@ return {
           },
         },
       }
-
-      -- Enable telescope extensions, if they are installed
       pcall(require('telescope').load_extension, 'fzf')
       pcall(require('telescope').load_extension, 'ui-select')
-
-      -- See `:help telescope.builtin`
       local builtin = require 'telescope.builtin'
       vim.keymap.set('n', '<leader>sh', builtin.help_tags, { desc = 'Help Tags' })
       vim.keymap.set('n', '<leader>sk', builtin.keymaps, { desc = 'Keymaps' })
@@ -143,26 +69,12 @@ return {
       vim.keymap.set('n', '<leader>sr', builtin.resume, { desc = 'Resume' })
       vim.keymap.set('n', '<leader>s.', builtin.oldfiles, { desc = 'Recent Files ("." for repeat)' })
       vim.keymap.set('n', '<leader>s<leader>', builtin.buffers, { desc = 'Existing Buffers' })
-
-      -- Slightly advanced example of overriding default behavior and theme
       vim.keymap.set('n', '<leader>s/', function()
-        -- You can pass additional configuration to telescope to change theme, layout, etc.
         builtin.current_buffer_fuzzy_find(require('telescope.themes').get_dropdown {
           winblend = 10,
           previewer = false,
         })
       end, { desc = 'Current Buffer' })
-
-      -- Also possible to pass additional configuration options.
-      --  See `:help telescope.builtin.live_grep()` for information about particular keys
-      -- vim.keymap.set('n', '<leader>s/', function()
-      --   builtin.live_grep {
-      --     grep_open_files = true,
-      --     prompt_title = 'Live Grep in Open Files',
-      --   }
-      -- end, { desc = '[S]earch [/] in Open Files' })
-
-      -- Shortcut for searching your Neovim configuration files
       vim.keymap.set('n', '<leader>sn', function()
         builtin.find_files { cwd = vim.fn.stdpath 'config' }
       end, { desc = 'Neovim' })
