b6768ff2543b4102eb02b02c8e45269b1e9f7587
513G3 <29024145+513G3@users.noreply.github.com>
2026-05-26 12:16:36 -0600

    Stop formatting on save

--- lua/plugins/conform.lua
+++ lua/plugins/conform.lua
@@ -1,7 +1,6 @@
 return {
   { -- Autoformat
     'stevearc/conform.nvim',
-    event = { 'BufWritePre' },
     cmd = { 'ConformInfo' },
     keys = {
       {
@@ -14,10 +13,6 @@ return {
     },
     opts = {
       notify_on_error = false,
-      format_on_save = {
-        timeout_ms = 500,
-        lsp_format = 'fallback',
-      },
       formatters_by_ft = {
         lua = { 'stylua' },
         proto = { 'buf' },
